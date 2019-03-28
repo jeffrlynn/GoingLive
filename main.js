12c8b53c6aa0d1e5e987662ffec97bc1e9ce840a
jeffrlynn <jeffrlynn@users.noreply.github.com>
2019-03-28 19:08:13 -0400

    Change style, add debugger

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const glob = require('glob')
 const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
